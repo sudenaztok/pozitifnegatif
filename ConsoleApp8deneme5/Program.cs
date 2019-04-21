0051083df5cac9ca1c567bdce7fc07a12a6e8fb2
data <data@DESKTOP-3P6AARG>
2019-04-21 15:14:06 +0300

    first commit

---
+++ ConsoleApp8deneme5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8deneme5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (0 < a)
+            {
+                Console.WriteLine("pozitif");
+            }
+            if (0>a)
+            {
+                Console.WriteLine("negatif");
+            }Console.ReadLine();
+        }
+	
+
+	}
+}
+
